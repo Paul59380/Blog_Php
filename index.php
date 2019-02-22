3628ce26aa902b01e33c785c1ace125d7255fb18
Paul59380 <paul.genly59380@gmail.com>
2019-02-22 14:54:04 +0100

    Separate html and php

--- index.php
+++ index.php
@@ -1,3 +1,14 @@
+<?php
+    try{
+        $db = new PDO('mysql:host=localhost;dbname=posts;charset=UTF8','root','',
+            array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
+    }catch (Exception $e){
+        die('Erreur : ' .$e ->getMessage());
+    }
+    $reply = $db -> query('SELECT id, title, content, DATE_FORMAT(creation_date, "%d%m%Y à %Hh:%imin:%ss")
+    AS new_date FROM tickets ORDER BY new_date LIMIT 0,10');
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -8,27 +19,18 @@
 <body>
     <h1>Bienvenue sur mon blog !</h1>
     <h3>Découvrez nos différents billet : </h3>
-    <?php
-        try{
-            $db = new PDO('mysql:host=localhost;dbname=posts;charset=UTF8','root','',
-                array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
-        }catch (Exception $e){
-            die('Erreur : ' .$e ->getMessage());
-        }
-        $reply = $db -> query('SELECT id, title, content, DATE_FORMAT(creation_date, "%d%m%Y à %Hh:%imin:%ss")
-        AS new_date FROM tickets ORDER BY new_date LIMIT 0,10');
 
+    <?php
         while($data = $reply ->fetch()){
             ?>
             <div class="billet">
                 <div class="titre">
-                    <span> <?php echo $data['title'] . ' <br> ' . $data['new_date']; ?> </span>
+                    <span> <?php echo htmlspecialchars($data['title']) . ' <br> ' . htmlspecialchars($data['new_date']); ?> </span>
                 </div>
-                <p> <?php echo $data['content'] ?><br>
+                <p> <?php echo htmlspecialchars($data['content']) ?><br>
                     <a href="comment.php?id=<?php echo $data['id'] ?>">Commentaires</a>
                 </p>
             </div>
-
     <?php
         }
     ?>
