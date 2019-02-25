f962d0409ce63983cc04f3b6a3743e0b0aadfb9d
Paul59380 <paul.genly59380@gmail.com>
2019-02-25 08:20:40 +0100

    Add templates

---
+++ template.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8" />
+        <title><?= $title ?></title>
+<link href="design.css" rel="stylesheet" />
+</head>
+
+<body>
+<?= $content ?>
+</body>
+</html>
