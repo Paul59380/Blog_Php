c98a360703cc3aec684929f241b754de1d5c6782
Paul59380 <paul.genly59380@gmail.com>
2019-02-25 14:34:19 +0100

    Modified controller in object

--- controller/frontend.php
+++ controller/frontend.php
@@ -1,23 +1,34 @@
 <?php
-require('model/frontend.php');
 
-function listPosts(){
-    $posts = getPosts(); // Must have the same name than the variable in display_home
+require_once ('model/CommentManager.php');
+require_once('model/PostManager.php');
+
+function listPosts()
+{
+    $postManager = new PostManager();
+    $posts = $postManager -> getPosts(); // Must have the same name than the variable in display_home
     require('view/display_home.php');
 }
 
-function post(){
-    $post = getPost($_GET['id']);
-    $comments = getComments($_GET['id']);
+function post()
+{
+    $postManager = new PostManager();
+    $commentManager = new CommentManager();
+
+    $post = $postManager -> getPost($_GET['id']);
+    $comments = $commentManager -> getComments($_GET['id']);
 
     require('view/comment.php');
 }
 
-function addComment($postId, $author, $postComments){
-    $affectLine = postComment($postId, $author, $postComments);
-    if($affectLine === false){
+function addComment($postId, $author, $postComments)
+{
+    $commentManager = new CommentManager();
+
+    $affectLine = $commentManager -> postComment($postId, $author, $postComments);
+    if ($affectLine === false) {
         die("Impossible d'ajouter ce commentaire !");
-    }else{
-        header('Location:index.php?action=post&id='. $postId);
+    } else {
+        header('Location:index.php?action=post&id=' . $postId);
     }
 }
